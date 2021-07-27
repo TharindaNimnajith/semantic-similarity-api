6634aaec26fe2a250da567d6f6858c3dcd892981
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-07-28 02:00:06 +0530

    test

--- controllers/main_controller.py
+++ controllers/main_controller.py
@@ -4,10 +4,12 @@ from . import textblob_sentiment
 
 
 def evaluate(answers):
-    spelling_score, grammar_score, matches = spelling_grammar_grammarbot.evaluate(answers.student_answer)
-    similarity_score = semantic_similarity.get_semantic_similarity(answers.student_answer,
-                                                                   answers.model_answer)['similarity'] * 10
-    sentiment_score = textblob_sentiment.evaluate(answers.student_answer)
+    model_answer = answers.model_answer
+    student_answer = answers.student_answer
+    spelling_score, grammar_score, matches = spelling_grammar_grammarbot.evaluate(student_answer)
+    similarity_score = semantic_similarity.get_semantic_similarity(student_answer,
+                                                                   model_answer)['similarity'] * 10
+    sentiment_score = textblob_sentiment.evaluate(student_answer)
     objectivity_score = 10 - sentiment_score.subjectivity * 10
     overall_score = calculate_overall_score(spelling_score,
                                             grammar_score,
@@ -15,8 +17,8 @@ def evaluate(answers):
                                             objectivity_score)
     return {
         'answers': {
-            'model_answer': answers.model_answer,
-            'student_answer': answers.student_answer
+            'model_answer': model_answer,
+            'student_answer': student_answer
         },
         'overall': overall_score,
         'scores': {
@@ -34,5 +36,5 @@ def evaluate(answers):
 
 
 def calculate_overall_score(spelling_score, grammar_score, similarity_score, objectivity_score):
-    overall_score = (spelling_score * 6 + grammar_score * 10 + similarity_score * 10 + objectivity_score * 4) / 30
+    overall_score = (spelling_score * 25 + grammar_score * 25 + similarity_score * 40 + objectivity_score * 10) / 100
     return overall_score
