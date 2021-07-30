616e5888b98e8de5b24c454ab880074bbb5a938e
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-07-31 03:14:07 +0530

    test

--- controllers/helpers/basic_metrics.py
+++ controllers/helpers/basic_metrics.py
@@ -1,7 +1,10 @@
 def get_comprehensiveness(word_limit,
                           word_count):
-    ratio = (word_count / word_limit) * 10
-    if ratio > 7.5:
-        return 10
+    if word_limit > 0:
+        ratio = (word_count / word_limit) * 10
+        if ratio > 7.5:
+            return 10
+        else:
+            return ratio
     else:
-        return ratio
+        return 0
